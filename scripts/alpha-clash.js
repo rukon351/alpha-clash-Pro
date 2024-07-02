1a24b7906062765bc5bb32f425edafc4784a41e5
rukon351 <rukonuzzaman.shiem.351@gmail.com>
2024-07-03 00:11:19 +0600

    show & hide element

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,8 +1,13 @@
+// function play() {
+//     // step -1 : hide the home screen. to hide the home screen add the class hidden to the home section
+//     const homeSection = document.getElementById('home-screen');
+//     homeSection.classList.add('hidden');
+//     // show the playground
+//     const playgroundSection = document.getElementById('play-ground');
+//     playgroundSection.classList.remove('hidden');
+// }
+
 function play() {
-    // step -1 : hide the home screen. to hide the home screen add the class hidden to the home section
-    const homeSection = document.getElementById('home-screen');
-    homeSection.classList.add('hidden');
-    // show the playground
-    const playgroundSection = document.getElementById('play-ground');
-    playgroundSection.classList.remove('hidden');
+    hideElementByid('home-screen');
+    showElementById('play-ground');
 }
