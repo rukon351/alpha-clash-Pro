82a91ea00e477ac530fa095e76fdc1a68106bf47
rukon351 <rukonuzzaman.shiem.351@gmail.com>
2024-07-05 00:50:07 +0600

    added js

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -7,10 +7,74 @@
 //     playgroundSection.classList.remove('hidden');
 // }
 
+function handleKeyboarKeyUpPress(event) {
+    const playerPessed = event.key;
+
+    // key player is expected to press
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+
+    // check right or wrong key pressed
+    if (playerPessed === expectedAlphabet) {
+        // console.log('you get a point');
+
+        const currentScore = getTextElementValueById('current-score');
+        // console.log(currentScore);
+        const updatedScore = currentScore + 1;
+        setTextElementValueById('current-score', updatedScore);
+
+
+        // -------------------------------------------
+        // update score:
+        // 1. get a current score
+        // const currentScoreElement = document.getElementById('current-score');
+        // const currentScoreText = currentScoreElement.innerText;
+        // const currentScore = parseInt(currentScoreText);
+
+
+        // // 2. increase new score
+        const newScore = currentScore + 1;
+
+        // // 3. show the update
+        // currentScoreElement.innerText = newScore;
+
+        // start a new round
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame();
+    }
+    else {
+        // console.log('you missed. you lost a life!');
+
+        const currentLife = getTextElementValueById('current-life');
+        const updatedLife = currentLife - 1;
+        setTextElementValueById('current-life', updatedLife);
+
+        if (updatedLife === 0) {
+            gameOver()
+        }
+
+        // ===========================
+        // step 1: get the current life number
+        // const currentLifeElement = document.getElementById('current-life');
+        // const currentLifeText = currentLifeElement.innerText;
+        // const currentLife = parseInt(currentLifeText);
+
+        // // step 2: reduce the life count
+        // const newLife = currentLife - 1;
+
+        // // step 3: display the updated life count
+        // currentLifeElement.innerText = newLife;
+    }
+}
+
+
+document.addEventListener('keyup', handleKeyboarKeyUpPress);
+
+
 function continueGame() {
     // step 1: genarate a random alphabet
     const alphabet = getARandomAlphabet();
-    console.log('your random alphabet is: ', alphabet)
 
     // set randomly generated alphabet to the screen (show it);
     const currentAlphabetElement = document.getElementById('current-alphabet');
@@ -22,7 +86,24 @@ function continueGame() {
 
 
 function play() {
+    // hide everything show only the playground
     hideElementByid('home-screen');
+    hideElementByid('final-score');
     showElementById('play-ground');
     continueGame();
+
+    // reset score and life
+    setTextElementValueById('current-life', 5);
+    setTextElementValueById('current-score', 0);
 }
+
+
+function gameOver() {
+    hideElementByid('play-ground');
+    showElementById('final-score');
+
+    // update final scoreeeeee
+    const lastScore = getTextElementValueById('current-score');
+    setTextElementValueById('last-score', lastScore);
+}
+
--- scripts/unility.js
+++ scripts/unility.js
@@ -14,6 +14,23 @@ function setBackgroundColorById(elementId) {
     element.classList.add('bg-orange-400');
 }
 
+function removeBackgroundColorById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+}
+
+
+function getTextElementValueById(elementId) {
+    const element = document.getElementById(elementId);
+    const elementValueText = element.innerText;
+    const value = parseInt(elementValueText);
+    return value;
+}
+
+function setTextElementValueById(elementId, value) {
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
 
 function getARandomAlphabet() {
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
